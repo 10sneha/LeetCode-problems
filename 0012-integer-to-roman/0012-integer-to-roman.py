f8300abe0cbbae0d062a968306782512e4473461
10sneha <92812710+10sneha@users.noreply.github.com>
2023-10-12 14:21:26 +0530

    Time: 47 ms (81.09%), Space: 16.2 MB (93.18%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,16 +1,8 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        d = {1:'I',5:'V',10:'X',50:'L',100:'C',500:'D',1000:'M',
-        4:'IV',9:'IX',40:'XL',90:'XC',400:'CD',900:'CM'}
+        ones = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+        tens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+        hrns = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+        ths = ["", "M", "MM", "MMM"]
 
-        result =''
-
-        for n in sorted(d.keys(),reverse=True):
-            while n <= num:
-                result += d[n]
-                num -= n
-        
-        return result
-
-
-        
+        return ths[int(num/1000)] + hrns[int((num%1000)/100)] +tens[int((num%100)/10)] +ones[int(num%10)]
