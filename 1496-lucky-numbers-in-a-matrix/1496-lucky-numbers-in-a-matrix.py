2a09774ddd41d09dae865da559f02dbae4019cd0
10sneha <92812710+10sneha@users.noreply.github.com>
2024-07-20 00:27:30 +0530

    Time: 118 ms (27.26%), Space: 16.9 MB (43.01%) - LeetHub

--- 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.py
+++ 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.py
@@ -1,13 +1,15 @@
 class Solution:
     def luckyNumbers (self, matrix: List[List[int]]) -> List[int]:
+        n = len(matrix)
+        m = len(matrix[0])
         rows = float('-inf')
-        for i in range(len(matrix)):
+        for i in range(n):
             row = min(matrix[i])
             rows = max(rows, row)
 
         cols = float('inf')
-        for i in range(len(matrix[0])):
-            col = max(matrix[j][i] for j in range(len(matrix)))
+        for i in range(m):
+            col = max(matrix[j][i] for j in range(n))
             cols = min(cols, col)
         
         if cols == rows:
